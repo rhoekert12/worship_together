b74d90d4df7bc0fdfeb631396c84952c85db1504
Ryley <ryley_hoekert@taylor.edu>
2016-09-14 19:03:44 -0400

    Created User model

---
+++ worship_together/spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe User do
+	let(:user) { User.new }
+	subject { user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:email) }
+end
