24e4faeba92fd7dbfb20d6d6d1eb3f6ff653c31b
Ryley <ryley_hoekert@taylor.edu>
2016-09-18 22:31:59 -0400

    First test passing 09/18/16

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     validates :name, presence: true
     validates :email, presence: true
     validates :password, presence: true
+    validates :name, length: { maximum: 50 }
+    validates :name, uniqueness: true
+    validates :email, uniqueness: true
 end
