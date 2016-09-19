a99bc0f9478d5c73e4eac37c58755e2a0fe93307
Ryley <ryley_hoekert@taylor.edu>
2016-09-18 20:26:13 -0400

    Adding another modified file from the merge

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-# encoding: UTF-8
->>>>>>> 626b2e71c43df32f4c662497f4cffdf1fc388a93
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
